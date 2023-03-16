c6b2ed04ee73b7e203e5105e131ccec55dc2c469
Christopher Beckham <christopher.j.beckham@gmail.com>
2023-03-16 14:09:00 -0400

    more cleanup

--- Volcano_train.py
+++ Volcano_train.py
@@ -5,6 +5,9 @@ import math
 import os 
 import argparse
 import json
+import pickle
+
+from scipy.stats import wasserstein_distance as w_distance
 
 from timeit import default_timer
 
@@ -45,16 +48,19 @@ def parse_args():
     parser = argparse.ArgumentParser(description="")
     #parser.add_argument('--datadir', type=str, default="")
     parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument("--val_batch_size", type=int, default=512,
+                        help="Batch size used for generating samples at inference time")
     parser.add_argument("--epochs", type=int, default=300)
-    parser.add_argument("--record_interval", type=int, default=10)
-    parser.add_argument("--L", type=int, default=100,
+    parser.add_argument("--record_interval", type=int, default=100)
+    parser.add_argument("--L", type=int, default=10,
                         help="Number of noise scales (timesteps)")
     parser.add_argument("--sigma_1", type=float, default=1.0)
     parser.add_argument("--sigma_L", type=float, default=0.01)
-    parser.add_argument("--eval_T", type=int, default=400,
+    parser.add_argument("--T", type=int, default=100,
                         help="The T parameter for annealed SGLD (how many iters per sigma)")
+    parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--npad", type=int, default=8)
-    parser.add_argument("--Ntest", type=int, default=5)
+    parser.add_argument("--Ntest", type=int, default=1024)
     parser.add_argument("--d_co_domain", type=float, default=32)
     parser.add_argument("--savedir", required=True, type=str)
     args = parser.parse_args()
@@ -104,6 +110,40 @@ class VolcanoDataset(Dataset):
                                                  self.x_train.min(), 
                                                  self.x_train.max())
 
+@torch.no_grad()
+def sample(fno, init_sampler, noise_sampler, sigma, n_examples, bs, T, 
+           fns=None):
+
+    buf = []
+    if fns is not None:
+        fn_outputs = {k:[] for k in fns.keys()}
+
+    n_batches = int(math.ceil(n_examples / bs))
+
+    print(n_batches, n_examples, bs, "<<<")
+
+    for _ in range(n_batches):
+        u = init_sampler.sample(bs)
+        res = u.size(1)
+        u = sample_trace(fno, noise_sampler, sigma, u, epsilon=2e-5, T=T) # (bs, res, res, 2)
+        u = u.view(bs,-1) # (bs, res*res*2)
+        u = u[~torch.any(u.isnan(),dim=1)]
+        #try:
+        u = u.view(-1,res,res,2) # (bs, res, res, 2)
+        #except:
+        #    continue
+        if fns is not None:
+            for fn_name, fn_apply in fns.items():
+                fn_outputs[fn_name].append(fn_apply(u).cpu())
+        buf.append(u.cpu())
+    buf = torch.cat(buf, dim=0)[0:n_examples]
+    # Flatten each list in fn outputs
+    if fns is not None:
+        fn_outputs = {k:torch.cat(v, dim=0)[0:n_examples] for k,v in fn_outputs.items()}
+    
+    assert len(buf) == n_examples
+    return buf, fn_outputs
+
 if __name__ == '__main__':
 
     args = DotDict(vars(parse_args()))
@@ -126,10 +166,11 @@ if __name__ == '__main__':
     fno = UNO(2+3, args.d_co_domain, s = s, pad=args.npad).to(device)
     print("# of trainable parameters: {}".format(count_params(fno)))
     fno = fno.to(device)
-    optimizer = torch.optim.Adam(fno.parameters(), lr=1e-3)
+    optimizer = torch.optim.Adam(fno.parameters(), lr=args.lr)
+    print(optimizer)
 
     # TODO: keep an eye on this
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 50, gamma=0.5)
+    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 50, gamma=0.5)
 
     # noise_sampler = PeriodicGaussianRF2d(s, s, alpha=1.5, tau=5, sigma=4.0, device=device)
     # init_sampler = PeriodicGaussianRF2d(s, s, alpha=1.1, tau=0.1, sigma=1.0, device=device)
@@ -149,6 +190,13 @@ if __name__ == '__main__':
     #plt.imshow(init_sampler.sample(1).cpu().view(s,s))
     #plt.colorbar()
     #plt.savefig('test.png')
+
+    # Compute the circular variance and skew on the training set
+    # and save this to the experiment folder.
+    var_train = circular_var(train_dataset.x_train).numpy()
+    skew_train = circular_skew(train_dataset.x_train).numpy()
+    with open(os.path.join(savedir, "gt_stats.pkl"), "wb") as f:
+        pickle.dump(dict(var=var_train, skew=skew_train), f)
 
     f_write = open(os.path.join(savedir, "results.json"), "a")
     for ep in range(args.epochs):
@@ -172,7 +220,7 @@ if __name__ == '__main__':
             # loss = ((h**2)/bsize)*((sigma[r]*torch.permute(fno(u + noise, sigma[r].view(1,1)), (0, 2, 3, 1)) + (1.0/sigma[r])*noise)**2).sum()
 
             # TODO: verify that this loss is correct
-            loss = ((h**2)/bsize)*((sigma[r]*fno(u + noise, sigma[r].view(1,1)) + (1.0/sigma[r])*noise)**2).sum()
+            loss = ((h**2)/bsize) * ((sigma[r]*fno(u + noise, sigma[r].view(1,1)) + (1.0/sigma[r])*noise)**2).sum()
 
             loss.backward()
             optimizer.step()
@@ -191,36 +239,37 @@ if __name__ == '__main__':
             #if iter_ == 10:
             #    break
 
-        pbar.close()
+        pbar.close()        
+        #scheduler.step()
 
-        buf = {k:np.mean(v) for k,v in buf.items()}
-        buf["epoch"] = ep
-        buf["lr"] = optimizer.state_dict()['param_groups'][0]['lr']
-        buf["sched_lr"] = scheduler.get_lr()[0] # should be the same as buf.lr
-        f_write.write(json.dumps(buf) + "\n")
-        f_write.flush()
-        print(json.dumps(buf))
-        
-        scheduler.step()
-
+        recorded = False
         if (ep + 1) % args.record_interval == 0:
             fno.eval()
-            with torch.no_grad():
-                u = init_sampler.sample(args.Ntest)
-                u = sample_trace(fno, noise_sampler, sigma, u, epsilon=2e-5, T=args.eval_T)
-                u = u.view(args.Ntest,-1)
-                u = u[~torch.any(u.isnan(),dim=1)]
-                try:
-                    u = u.view(-1,s,s,2)
-                except:
-                    continue
-                
+            recorded = True
+
+            u, fn_outs = sample(
+                fno, init_sampler, noise_sampler, sigma, 
+                bs=args.val_batch_size, n_examples=args.Ntest, T=args.T,
+                fns={"skew": circular_skew, "var": circular_var}
+            )
+            skew_generated = fn_outs['skew']
+            var_generated = fn_outs['var']
+            # Dump this out to disk as well.
+            with open(os.path.join(savedir, "stats.pkl"), "wb") as f:
+                pickle.dump(dict(var=var_generated, skew=skew_generated), f)
+
+            w_skew = w_distance(skew_train, skew_generated)
+            w_var = w_distance(var_train, var_generated)
+            w_total = w_skew + w_var
+
+            print("w total:", w_total)
+
+            #import pdb; pdb.set_trace()
 
             #stats[k, (ep+1)//record_int -1, 0] = train_err
             #stats[k, (ep+1)//record_int -1, 1] = max_err
             #stats[k, (ep+1)//record_int -1, 2] = l2_err
             #stats[k, (ep+1)//record_int -1, 3] = Ntest - u.size(0)
-
             
             path  = os.path.join(savedir,
                                  'ns_noise_400_point1_noise_15_1_UNO_init',
@@ -239,27 +288,49 @@ if __name__ == '__main__':
 
             torch.save(fno.state_dict(), path + 'model.pt')
 
-            u = u.cpu()
+            #u = u.cpu()
             # for j in range(u.size(0)):
             #     plt.figure(j)
             #     plt.imshow(u[j,:,:].view(s,s))
             #     plt.colorbar()
             #     plt.savefig(path + str(j) + '.png')
             #     plt.close()
-            fig, ax = plt.subplots(1, args.Ntest, figsize=(16,4))
-            for j in range(u.size(0)):
-                phase = torch.atan2(u[j,:,:,1], u[j,:,:,0]).cpu().detach().numpy()
+
+            #with torch.no_grad():
+            #    var_generated = circular_var(u).numpy()
+            #    skew_generated = circular_skew(u).numpy()
+
+            Nplot = 5
+            u_subset = u[0:Nplot]
+            fig, ax = plt.subplots(1, Nplot, figsize=(16,4))
+            for j in range(u_subset.size(0)):
+                phase = torch.atan2(u_subset[j,:,:,1], u_subset[j,:,:,0]).cpu().detach().numpy()
                 phase = (phase + np.pi) % (2 * np.pi) - np.pi
                 bar = ax[j].imshow(phase,  cmap='RdYlBu', vmin = -np.pi, vmax=np.pi,extent=[0,1,0,1])
-            cax = fig.add_axes([ax[args.Ntest-1].get_position().x1+0.01,ax[args.Ntest-1].get_position().y0,0.02,ax[args.Ntest-1].get_position().height])
+            cax = fig.add_axes([ax[Nplot-1].get_position().x1+0.01,
+                                ax[Nplot-1].get_position().y0,0.02,
+                                ax[Nplot-1].get_position().height])
             plt.colorbar(bar, cax=cax) # Similar to fig.colorbar(im, cax = cax)
             # print(path+'.pdf')
-            plt.savefig(path_Figure + str(ep+1)+'.pdf')  
+            plt.savefig(path_Figure + str(ep+1)+'.pdf', bbox_inches='tight')
             
             #print(ep+1, train_err, default_timer() - t1)
             
         else:
             #print(ep+1, train_err, default_timer() - t1)
             pass
+
+        buf = {k:np.mean(v) for k,v in buf.items()}
+        buf["epoch"] = ep
+        buf["lr"] = optimizer.state_dict()['param_groups'][0]['lr']
+        #buf["sched_lr"] = scheduler.get_lr()[0] # should be the same as buf.lr
+        if recorded:
+            buf["w_skew"] = w_skew
+            buf["w_var"] = w_var
+            buf["w_total"] = w_skew + w_var
+        f_write.write(json.dumps(buf) + "\n")
+        f_write.flush()
+        print(json.dumps(buf))
+
 
     #scipy.io.savemat('gm_trace/stats.mat', {'stats': stats})
